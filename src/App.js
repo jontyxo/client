4a5e2eca4bd03cf4a2b336578ab20510f42b0373
jontyxo <joyesh16wa@gmail.com>
2025-06-19 01:46:42 +0530

    sd

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function AppWrapper() {
     setIncomingReport(null);
   };
 useEffect(() => {
-    const socket = new WebSocket('ws://citymall-sldm.onrender.com/');
+    const socket = new WebSocket('wss://citymall-sldm.onrender.com/');
 
     socket.onmessage = (event) => {
       const msg = JSON.parse(event.data);
